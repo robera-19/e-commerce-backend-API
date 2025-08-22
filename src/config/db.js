d6c2769e803a079f772df37df7fd60ab56c31085
robera <roberawakjira1@gmail.com>
2025-08-22 12:47:46 -0700

    feat: modified from ES6 to commonjs form

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,14 @@
-module.exports = {
-    PORT: process.env.PORT || 3000,
-    DB_URI: process.env.DB_URI,
+const mongoose = require("mongoose");
+require("dotenv").config(); // Loads the .env file
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected Successfully!");
+  } catch (err) {
+    console.log("Error connecting to the database:", err.message);
+    process.exit(1);
+  }
 };
+
+module.exports = connectDB;
